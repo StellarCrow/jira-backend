3d226e910a3ac61fe2f5b7ea63288b05280d8050
StellarCrow <dariadar12@gmail.com>
2020-05-13 00:51:24 +0300

    add travis

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Application } from 'express';
 import authRouter from './src/routes/api/auth.route';
 
 const app: Application = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/api', authRouter);
 
@@ -9,6 +10,6 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('App is listening on port 3000!');
 });

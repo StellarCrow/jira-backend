4abcef02e67236fc774c09154d6c79df0ea8547a
StellarCrow <dariadar12@gmail.com>
2020-05-13 11:59:03 +0300

    add connection to mongodb

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response, Application } from 'express';
+import mongoose from 'mongoose';
 import authRouter from './src/routes/api/auth.route';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/jira';
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use('/api', authRouter);
 
